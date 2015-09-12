95f9aceba9940497fd4d2d5efd18d60b03ca0860
Sandeep Kulkarni <sandeep.kul88@gmail.com>
2015-09-11 23:07:32 -0700

    Both solutions for : algorithm to determine if a string has all unique characters
    Both solutions for : algorithm to determine if a string has all unique
    characters

--- src/datastructures/arraystrings/CheckIfStringContainsUniqueCharacters.java
+++ src/datastructures/arraystrings/CheckIfStringContainsUniqueCharacters.java
@@ -1,10 +1,62 @@
 package datastructures.arraystrings;
 
+import java.util.Scanner;
+
+/**
+Implement an algorithm to determine if a string has all unique characters. What
+if you cannot use additional data structures?
+ * 
+ */
 public class CheckIfStringContainsUniqueCharacters {
 
 	public static void main(String[] args) {
-		
+		Scanner s = new Scanner(System.in);
 
+		String inputString = s.nextLine();
+
+		//complexity - O(n*2), n = length of string
+		boolean isUnique_1 = isUniqueChar_1(inputString);
+
+		System.out.println("Is Unique = " + isUnique_1);
+
+		//Better solution : Complexity - O(n), n = length of string
+		boolean isUnique_2 = isUniqueChar_2(inputString);
+
+		System.out.println("Is Unique = " + isUnique_2);
+
+		s.close();
+	}
+
+	private static boolean isUniqueChar_1(String inputString){
+		boolean isUnique = true;
+
+		for(int i = 0; i < inputString.length(); i++) {
+
+			for(int j = i + 1; j < inputString.length(); j++) {				
+				if(inputString.charAt(i) == inputString.charAt(j)) {
+					isUnique = false;
+					break;
+				}
+			}
+		}
+		return isUnique;
+	}
+
+	private static boolean isUniqueChar_2(String inputString){
+
+		boolean[] boolArray = new boolean[256];
+
+		for(int i = 0; i < inputString.length(); i++) {
+			int charValueIndex = inputString.charAt(i);
+
+			//if true (i.e true value was set before at that index), so it means char value at that index is present in array
+			if(boolArray[charValueIndex]){
+				return false;				
+			}
+			//char not present at the index, so set value as true
+			boolArray[charValueIndex] = true;
+		}
+		return true;
 	}
 
 }
