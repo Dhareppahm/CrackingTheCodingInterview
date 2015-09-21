a5ee943ca4e9c897af9e06ae6358603e6d117dcf
Sandeep Kulkarni <sandeep.kul88@gmail.com>
2015-09-21 00:00:04 -0700

    StringReplace solution

---
+++ src/datastructures/arraystrings/StringReplaceSpace.java
@@ -0,0 +1,33 @@
+package datastructures.arraystrings;
+
+import java.util.Scanner;
+
+public class StringReplaceSpace {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		String inputString = s.nextLine();
+
+		//1. using inbuilt function 
+		String newInputString = inputString.trim().replaceAll("\\s", "%20");
+		System.out.println(newInputString);
+
+
+		//2. Using char array
+		char[] chars = inputString.trim().toCharArray();
+		StringBuilder sb = new StringBuilder();
+
+		for(char c : chars){
+			if(c == ' '){
+				sb.append("%20");
+			}else{
+				sb.append(c);
+			}
+		}
+		System.out.println(sb.toString());
+
+		s.close();
+	}
+
+}
