f3cbf686c9b9b2468a4ab3b08c962345976f453b
Sandeep Kulkarni <sandeep.kul88@gmail.com>
2016-07-18 22:07:12 -0700

    Is Unique characters

--- src/datastructures/arraystrings/IsUnique.java
+++ src/datastructures/arraystrings/IsUnique.java
@@ -2,13 +2,21 @@ package datastructures.arraystrings;
 
 import java.util.Scanner;
 
-/** 
- * Problem 1.1
-Implement an algorithm to determine if a string has all unique characters. What
-if you cannot use additional data structures?
- * 
+/**
+Problem 1.1 : Is Unique
+Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+ ----------------------------------------------------------------------------------------------------------------------------
+ Approach:
+ 1. Create a boolean array, and store true at index location corresponding to character in string.
+ 2. If character occurs again, the value would be true at the same index, and we will know char is repeated.
+ Time complexity: O(n), Space complexity : O(1)
+
+ If we cannot use additional Space:
+ 1. Brute Force: O(n^2) - Check a character with all other characters in string
+ 2. Sort string and do linear search - O(n log n)
+
  */
-public class CheckIfStringContainsUniqueCharacters {
+public class IsUnique {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
@@ -31,19 +39,17 @@ public class CheckIfStringContainsUniqueCharacters {
 
 	
 	/**
-	 * Checks a char with every other character in string. So complexity is O(n*2)
+	 * Brute Force: Checks a char with every other character in string. So complexity is O(n^2)
 	 */
 	private static boolean isUniqueChar_BruteForce(String inputString) {		
-		boolean isUnique = true;
 		for(int i = 0; i < inputString.length(); i++) {
 			for(int j = i + 1; j < inputString.length(); j++) {				
-				if(inputString.charAt(i) == inputString.charAt(j)) {
-					isUnique = false;
-					break;
+				if(inputString.charAt(i) == inputString.charAt(j)) {    //if character match
+					return false;
 				}
 			}
 		}
-		return isUnique;
+		return true;
 	}
 
 	/**
